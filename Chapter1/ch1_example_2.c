5d49606a03c07be0be171a2fdc5b35df79219f9b
dongiks <ikspurt0505@gmail.com>
2018-07-19 22:43:29 +0900

    chapter 1 example code

---
+++ Chapter1/ch1_example_2.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+int main() {
+	printf("%d을 %d로 나누면 %lf입니다.", 1, 2, 0.5);
+}
---
+++ Chapter1/ch1_example_3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main() {
+	/*
+	printf("Hello, world!\n");
+	*/
+	printf("Be\rHappy!\nBaby");
+
+	return 0;
+
+}
+/*
+	//예상되는 출력물
+
+	Happy!
+	Baby
+*/
+
+
+
+
+/*
+	printf 함수로 제어 문자 출력
+
+	"\n" 줄을 바꾼다.
+	"\t" 출력 위치를 다음 탭으로 위치를 옮긴다.
+	"\r" 출력 위치를 줄의 맨 앞으로 옮긴다.
+	"\b" 출력 위치를 한 칸 왼쪽으로 옮긴다.
+	"\a" 경보 벨소리를 낸다.
+*/
